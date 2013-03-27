14ab66fa20c30fd64feadc5b2715415f1605f7d0
root <root@arduinocam.com>
2013-03-27 16:35:01 -0400

    Added a 3-sec sleep to give 'Motion' time to gracefully quit.
    Originally used Open4 to kill motion, but because of the zombie processes, I will change this.

--- app/controllers/rubytest_controller.rb
+++ app/controllers/rubytest_controller.rb
@@ -53,16 +53,23 @@ class RubytestController < ApplicationController
     
     if pid_exists
       # If PID file exists, destroy process first so that it has a graceful quit
-      destroy_process = system("kill $(pidof motion)")
-      
+      #destroy_process = system("kill $(pidof motion)")
+      pid, stdin, stdout, stderr = Open4::popen4 "killall motion"
       # Check again if the motion.pid exists
       # If it does, then process wasn't killed gracefully
+      # Potential for infinite loop here
+      #while (pid_still_exists = File.exist?("/var/www/test/pid/motion.pid"))
+        # Loop until PID no longer exists?
+      #end
+      
+      sleep 3.0
+      
       pid_still_exists = File.exist?("/var/www/test/pid/motion.pid")
       
       if pid_still_exists
         # Remove the file if it exists
         remove_pid_forcefully = File.delete("/var/www/test/pid/motion.pid")
-        
+
         if remove_pid_forcefully
           @status = "PID file removed forcefully."
           @button_label = "Start Motion"  
@@ -92,11 +99,16 @@ class RubytestController < ApplicationController
     if !pid_exists
       
       start = system("/usr/local/bin/motion -m")
+      #pid, stdin, stdout, stderr = Open4::popen4 "/usr/local/bin/motion -m"
+      
+      sleep 1.1
       
       @pid_now_exists = File.exist?("/var/www/test/pid/motion.pid")
       
       if @pid_now_exists
-        @status = "Static Camera Started."
+        pid, stdin, stdout, stderr = Open4::popen4 "pidof -s /usr/local/bin/motion"
+        getpid = stdout.read.strip
+        @status = "Motion started: PID #{getpid}"
         @button_label = "Close Motion."
       else
         @status = "Motion failed to start."
@@ -104,7 +116,8 @@ class RubytestController < ApplicationController
       end
       
     else
-      @status = "Motion is already running."
+      pid = Process.pid("motion")
+      @status = "Motion is already running: PID #{pid}"
       @button_label = "Close Motion"
     end
     
