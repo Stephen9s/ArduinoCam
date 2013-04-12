7ebb6e23fc31db07db51b43457434c906369e9c7
root <root@arduinocam.com>
2013-04-11 21:20:46 -0400

    Updated secret token.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,1 +1,7 @@
-Removed secret token. Will regenerate.
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test::Application.config.secret_token = '04405525d51e261697949a9567ecf1469162264aa2d9625493b259763e6ad67d6be84a77bf42db63e6f22edb40e3223556dd50b10a61da03e112daa261a9ded6'
